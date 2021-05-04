a40867b27132192c1d5864d99408edf802e8a268
Maksim Panin <me@mpanin.me>
2021-05-04 11:24:49 +0300

    add file for fork process

---
+++ week5/2_fork_process.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import time
+import os
+
+pid = os.fork()
+
+if pid == 0:
+    while True:
+        print("child:", os.getpid())
+        time.sleep(5)
+else:
+    print("parent:", os.getpid())
+    os.wait()
